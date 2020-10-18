4fd13e34c2d2e340b5b310ed3d629a9ea853edba
OverlandMeat96 <55293932+OverlandMeat96@users.noreply.github.com>
2020-10-17 20:57:52 -0400

    adding AJAX content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 (() => {
 
+    function handleData (data){
+        let cn = document.querySelector('.coursename'),
+            cc = document.querySelector('.coursecode'),
+            pn = document.querySelector('.profname'),
+            ct = document.querySelector('.list-inline'),
+            cd = document.querySelector('.coursedesc');
+
+            cn.innerText = data.coursename+" - "+data.coursecode;
+            cc.innerText = data.coursename+" - "+data.coursecode;
+            pn.innerText = "Professor - "+data.profname;
+            ct.innerText = " 🕒 "+data.classtime[0]+" 🕒 "+data.classtime[1];
+            cd.innerText = data.coursedesc;
+    }
+
     // make aan AJAX request using the Fetch API
     fetch('./data/classData.json')
         .then(res => res.json())
@@ -8,9 +22,12 @@
 
         //handleData
         //here's where you would call the function that puts the pieces on the page
+        handleData(data);
+
+
          })
     .catch((err) => {
-        console.log(err);
+        console.log(err + '\n' + "error: 404. Could not load data.");
     })
 
 })();
